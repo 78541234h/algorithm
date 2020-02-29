593c8f717fb175e53cdbe43d3cb3182fff5178d6
78541234h <define_hu@163.com>
2020-02-29 16:03:37 +0800

    2020 3 1

---
+++ src/chapter5/FindStrInMayNullStrArr.java
@@ -0,0 +1,39 @@
+package chapter5;
+
+public class FindStrInMayNullStrArr {
+    public static void main(String[] args) {
+    }
+
+    public static void test() {
+
+    }
+
+    public static int findPos(String[] arr, String s) {
+        int left = 0;
+        int res = -1;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] != null) {
+                if (arr[mid].equals(s)) {
+                    res = mid;
+                    right = mid - 1;
+                } else if (arr[mid].compareTo(s) > 0) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                int i = mid;
+                while (--i >= left && arr[i] == null) ;
+                if (i < left || arr[i].compareTo(s) < 0) {
+                    left = mid + 1;
+                } else {
+                    res = s.equals(arr[i]) ? i : res;
+                    right = i - 1;
+                }
+            }
+        }
+        return res;
+    }
+}
--- src/chapter5/JudgeUniqueCharInString.java
+++ src/chapter5/JudgeUniqueCharInString.java
@@ -1,4 +1,102 @@
 package chapter5;
 
+import utils.PrintUtil;
+
 public class JudgeUniqueCharInString {
+    public static void main(String[] args) {
+        test();
+
+    }
+
+    public static void test() {
+        char[] arr = {'1', '6', '4', '2', '5', '8', '9', '7', '3'};
+        PrintUtil.print(judgeCharUnique(arr));
+    }
+
+    //T(N)
+    //扩展ASCII码有256个，ASCII有128个
+    public static boolean judgeCharUnique(String s) {
+        if (s == null || s.length() < 2) return true;
+        boolean[] map = new boolean[256];
+        for (int i = 0; i < s.length(); i++) {
+            if (map[s.charAt(i)]) return false;
+            map[s.charAt(i)] = true;
+        }
+        return true;
+    }
+
+    //T(nlogn) S(1)
+    public static boolean judgeCharUnique(char[] arr) {
+        if (arr == null || arr.length < 2) return true;
+        heapsort(arr);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == arr[i - 1]) return false;
+        }
+        return true;
+    }
+
+    private static void heapsort(char[] arr) {
+        for (int i = arr.length / 2 - 1; i >= 0; i--) {
+            shiftDown(arr, i, arr.length - 1);
+        }
+        for (int i = arr.length - 1; i > 0; i--) {
+            char tmp = arr[i];
+            arr[i] = arr[0];
+            arr[0] = tmp;
+            shiftDown(arr, 0, i - 1);
+        }
+    }
+
+    private static void shiftDown(char[] arr, int from, int lower) {
+        int lc = from * 2 + 1;
+        char tmp = arr[from];
+        while (lc <= lower) {
+            if (lc + 1 <= lower && arr[lc] > arr[lc + 1])
+                lc = lc + 1;
+            if (tmp < arr[lc]) break;
+            arr[from] = arr[lc];
+            from = lc;
+            lc = from * 2 + 1;
+        }
+        arr[from] = tmp;
+
+    }
+
+
+    public static int KMP(String sl, String ss) {
+        if (ss == null || ss.length() == 0) return 0;
+        if (sl == null || sl.length() == 0) return -1;
+        int[] help = help(ss);
+        int i = 0, j = 0;
+        while (i < sl.length() && j < ss.length()) {
+            if (sl.charAt(i) == ss.charAt(j)) {
+                i++;
+                j++;
+            } else if (j == 0) {
+                i++;
+            } else {
+                j = help[j];
+            }
+        }
+        return j == ss.length() ? i - j : -1;
+    }
+
+    private static int[] help(String s) {
+        int[] help = new int[s.length()];
+        help[0] = -1;
+        if (s.length() > 2) {
+            int pos = 2;
+            int cn = 0;
+            while (pos < s.length()) {
+                if (s.charAt(pos - 1) == s.charAt(cn)) {
+                    help[pos++] = ++cn;
+                } else if (cn == 0) {
+                    help[pos++] = 0;
+                } else {
+                    cn = help[cn];
+                }
+            }
+        }
+        return help;
+    }
 }
